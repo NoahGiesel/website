56dd14bdeee2e0be7821f32374ea534a99b19f3c
Noah Giesel <44894110+NoahGiesel@users.noreply.github.com>
2019-02-06 16:53:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+ var primo = document.getElementById('name');
+
+ var secondo = document.getElementById('surname');
+
+
+function showContent(){
+    var userData = [];
+    userData.push(primo.value);
+    userData.push(secondo.value);
+    console.log(userData); 
+}
