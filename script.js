7ec3c6b6e98d7d4110182b4f5e8fd1503e6be8bd
Noah Giesel <44894110+NoahGiesel@users.noreply.github.com>
2019-05-11 21:33:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
- var primo = document.getElementById('name');
-
- var secondo = document.getElementById('surname');
 
 
-function showContent(){
-    var userData = [];
-    userData.push(primo.value);
-    userData.push(secondo.value);
-    console.log(userData); 
-}
+var toggler = false ; 
+
+
+function toggle() {
+
+     var el = document.getElementById("ham");
+    var men = document.getElementById("menu");
+     
+    if(!toggler){  
+      el.classList.toggle("active");
+      men.style.width = '500px'
+      men.style.height = '500px'
+      toggler = !toggler ; 
+    } else { 
+
+      el.classList.toggle("active");
+      men.style.width = '0px'
+      men.style.height = '0px'
+
+      toggler = !toggler ; 
+    }
+       
+ 
+
+
+  }
+  
